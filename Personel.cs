34d67a44c31646e83b475b39adf82e035a5aca4b
ASUS <ASUS@ISMAIL>
2022-10-27 19:27:37 +0300

    Add project files.

---
+++ Personel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Encapsulation_3
+{
+    public class Personel
+    {
+        public string CompanyName { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+
+        public string Email
+        {
+            get
+            {
+                return $"{Name.ToLower()}.{SurName.ToLower()}@{CompanyName.ToLower()}.com";
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2_Encapsulation_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel p = new Personel();
+            p.Name = "Kübra";
+            p.SurName = " Karakus";
+            p.CompanyName = "bt akademi";
+
+            Console.WriteLine($"{p.Name} - {p.SurName} - {p.Email}");
+
+            Personel p1 = new Personel();
+            Console.WriteLine("Personel adı giriniz:");
+            p1.Name = Console.ReadLine();
+
+            Console.WriteLine("Personel SoyAdı Giriniz");
+            p1.Name = Console.ReadLine();
+            p1.CompanyName = "btakademi";
+
+            Console.WriteLine($"Personelin Email Adresi = {p1.Email} ");
+
+
+            Console.ReadKey();
+        }
+    }
+}
